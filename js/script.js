0af138e462ceeae74133e003d716d93853ffd750
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-11-21 15:50:41 -0500

    Adds starter files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const colors = ['red', 'blue', 'green', 'purple', 'orange', 'gold', 'grey', 'black']
+const icons = ['forbidden', 'printer', 'menu', 'bell', 'briefcase', 'calendar', 'camera', 'microphone']
+
+const maker = {
+  icon: 'forbidden',
+  color: 'red'
+}
+
+const $icon = document.getElementById('icon')
+const $icons = document.getElementById('icons')
+const $colors = document.getElementById('colors')
