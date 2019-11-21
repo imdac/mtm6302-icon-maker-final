54a40b727e9373ee1d4f6305c75545af88ced80a
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-11-21 15:57:11 -0500

    Creates a final version of the project

--- js/script.js
+++ js/script.js
@@ -9,3 +9,37 @@ const maker = {
 const $icon = document.getElementById('icon')
 const $icons = document.getElementById('icons')
 const $colors = document.getElementById('colors')
+
+const iconButtons = []
+
+for (const icon of icons) {
+  iconButtons.push(`<div class="icon-small-${icon}" data-icon="${icon}"></div>`)
+}
+
+$icons.innerHTML = iconButtons.join('')
+
+const colorButtons = []
+
+for (const color of colors) {
+  colorButtons.push(`<div class="color-${color}" data-color="${color}"></div>`)
+}
+
+$colors.innerHTML = colorButtons.join('')
+
+function createIcon () {
+  $icon.className = `icon-${maker.icon}-${maker.color}`
+}
+
+$icons.addEventListener('click', function (e) {
+  if (e.target.dataset.icon) {
+    maker.icon = e.target.dataset.icon
+    createIcon()
+  }
+})
+
+$colors.addEventListener('click', function (e) {
+  if (e.target.dataset.color) {
+    maker.color = e.target.dataset.color
+    createIcon()
+  }
+})
